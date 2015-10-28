cc4d2537447c4ddaa69dbe0f2b1aa1d3d4ca4614
janek <janek.thomas@web.de>
2015-10-28 22:30:51 +0100

    check for user_id class

--- R/send_message.R
+++ R/send_message.R
@@ -1,7 +1,18 @@
 send_message <- function(message, chat_id, token){
   
-  httr::POST(url = paste0("https://api.telegram.org/bot", token, "/sendmessage"),
-             body = list(chat_id = chat_id, text = "nice"))
+  if(!(inherits(chat_id, "chat_id") | is.character(chat_id))){
+    error("chat_id must be a chat_id object or a character string")
+  }
+
+  
+  if(inherits(chat_id, "chat_id")){
+    chat_id <- chat_id$chat_id 
+  }
+  
+  
+  httr::POST(url = paste0("https://api.telegram.org/bot", token, 
+                          "/sendmessage"), body = list(chat_id = chat_id, 
+                                                       text = "nice"))
   
 }
 
