4c9c3f32d595602c0fddbf8921b4a746d4b46b91
janek <janek.thomas@web.de>
2015-10-28 22:11:48 +0100

    modified chat_id object and added print method

---
+++ R/print.chat_id.R
@@ -0,0 +1,3 @@
+print.chat_id <- function(x){
+  print(paste("Chat id for user", x$username, "- created on", x$created_on))
+}
--- R/recieve_chat_id.R
+++ R/recieve_chat_id.R
@@ -1,12 +1,20 @@
 recieve_chat_id <- function(token){
   
+  chat_id <- list()
+  
   x <- fromJSON(paste0("https://api.telegram.org/bot", token, "/getUpdates"))
   
-  if(x$ok){
-    x$result$message$from[NROW(x$result$message$from), ]
-  }
-  else{
+  if(!x$ok){
     warning("no message found")
   }
+  else{
+    chat_id$chat_id <- x$result$message$from[NROW(x$result$message$from), 1]
+    chat_id$username <- x$result$message$from[NROW(x$result$message$from), 2]
+    chat_id$created_on <- as.POSIXct(tail(x$result$message$date, 1), 
+                                     origin="1970-01-01")
+  }
   
+  class(chat_id) <- "chat_id"
+  chat_id
 }
+
