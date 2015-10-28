119998c84c8a7852b8b279571841e14869b3be14
janek <janek.thomas@web.de>
2015-10-28 23:05:15 +0100

    added check for chat_bot object and collapse for vector messegas

--- R/send_message.R
+++ R/send_message.R
@@ -1,18 +1,29 @@
-send_message <- function(message, chat_id, token){
+send_message <- function(message, chat_id, token, collapse = TRUE){
   
   if(!(inherits(chat_id, "chat_id") | is.character(chat_id))){
     error("chat_id must be a chat_id object or a character string")
   }
 
+  if(!(inherits(token, "chat_bot") | is.character(chat_id))){
+    error("token must be a chat_bot object or a character string")
+  }
   
   if(inherits(chat_id, "chat_id")){
     chat_id <- chat_id$chat_id 
   }
   
+  if(inherits(token, "chat_bot")){
+    token <- token$token 
+  }
+  
+  if(collapse){
+    message <- paste(message, collapse = ", ")
+  }
+  
   
   httr::POST(url = paste0("https://api.telegram.org/bot", token, 
                           "/sendmessage"), body = list(chat_id = chat_id, 
-                                                       text = "nice"))
+                                                       text = message))
   
 }
 
